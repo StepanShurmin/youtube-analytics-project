69da9d694f5934f30360a8115a332bcf09ab9908
StepanShurmin <shurmin.sa@gmail.com>
2023-07-19 10:44:53 +0400

    Домашка № 6

--- src/video.py
+++ src/video.py
@@ -12,14 +12,21 @@ class Video:
         """Инициализирует экземпляр класса Video."""
         self.__video_id = video_id
         self.verify_video_id(video_id)
-        self.video_response = Video.get_service().videos().list(
-            part='snippet,statistics,contentDetails,topicDetails',
-            id=self.__video_id
-        ).execute()
-        self.title = self.video_response['items'][0]['snippet']['title']
-        self.url = f"https://www.youtube.com/watch?v={self.__video_id}"
-        self.view_count = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+        try:
+            self.video_response = Video.get_service().videos().list(
+                part='snippet,statistics,contentDetails,topicDetails',
+                id=self.__video_id
+            ).execute()
+            self.title = self.video_response['items'][0]['snippet']['title']
+            self.url = f"https://www.youtube.com/watch?v={self.__video_id}"
+            self.view_count = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+
+        except:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self) -> str:
         """Возвращает строковое представление экземпляра."""
